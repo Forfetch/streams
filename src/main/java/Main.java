d440129c21957f084f5234a4164d43393bc33b2b
mabuttta <mabutta@gmail.com>
2023-05-03 19:22:41 +0300

    ver 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
+// first lesson
+//        Stream.of(1,2,3)
+//                .forEach(System.out::println);
+//
+//        String str = Math.random() > 0.5 ? "Im feeling lucky" : null;
+//        Stream.ofNullable(str)
+//                .forEach(System.out::println);
+//
+//        Stream.generate(() -> 6)
+//                .limit(6)
+//                .forEach(System.out::println);
 
     }
 }
