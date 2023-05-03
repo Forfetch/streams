f1a532dc4daab4bcaa2e7fae03e82fa5d80c1355
mabuttta <mabutta@gmail.com>
2023-05-03 20:02:02 +0300

    ver 1.3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,57 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
-        Stream.iterate(1, x -> x * 2)
-                .limit(6)
-                .forEach(System.out::print);
-
-        Stream.iterate(2, x -> x < 25, x -> x + 6)
-                .forEach(System.out::println);
-
-        Stream.iterate(4, x -> x < 100, x -> x * 4)
-                .forEach(System.out::println);
-
-        Stream.concat(
-                        Stream.of(1, 2, 3, 4),
-                        Stream.of(5, 6, 7, 8))
-                .forEach(System.out::println);
-
-
-        IntStream.range(0, 10)
-                .forEach(System.out::println);
-        LongStream.range(-10l, -5l)
-                .forEach(System.out::println);
+//  3 lesson
+//        Stream.of(120, 410, 85, 32, 314, 12)
+//                .filter(x -> x > 100)
+//                .forEach(System.out::println);
+//
+//        IntStream.range(2, 9)
+//                .filter(x -> x % 3 == 0)
+//                .forEach(System.out::println);
+//
+//        Stream.of("1", "2", "3", "4")
+//                .map(Integer::parseInt)
+//                .map(x -> x + 10)
+//                .forEach(System.out::println);
+//
+//        Stream.of(120, 410, 85, 32, 314, 12)
+//                .map(x->x+11)
+//                .forEach(System.out::println);
+//
+//        Stream.of("10","11","32")
+//                .map(x -> Integer.parseInt(x, 16))
+//                .forEach(System.out::println);
+//
+//        Stream.of(120,410,85,32,314,12)
+//                .limit(2)
+//                .limit(5)
+//                .forEach(System.out::println);
+//        Stream.of(19)
+//                .limit(0)
+//                .forEach(System.out::println);
+//
+// Secnd lesson
+//        Stream.iterate(1, x -> x * 2)
+//                .limit(6)
+//                .forEach(System.out::print);
+//
+//        Stream.iterate(2, x -> x < 25, x -> x + 6)
+//                .forEach(System.out::println);
+//
+//        Stream.iterate(4, x -> x < 100, x -> x * 4)
+//                .forEach(System.out::println);
+//
+//        Stream.concat(
+//                        Stream.of(1, 2, 3, 4),
+//                        Stream.of(5, 6, 7, 8))
+//                .forEach(System.out::println);
+//
+//
+//        IntStream.range(0, 10)
+//                .forEach(System.out::println);
+//        LongStream.range(-10l, -5l)
+//                .forEach(System.out::println);
 // first lesson
 //        Stream.of(1,2,3)
 //                .forEach(System.out::println);
