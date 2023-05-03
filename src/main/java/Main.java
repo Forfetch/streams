15a77d0613a94b60828ad293d039ff6a0984b319
mabuttta <mabutta@gmail.com>
2023-05-03 19:37:56 +0300

    ver 1.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,30 @@
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+
+        Stream.iterate(1, x -> x * 2)
+                .limit(6)
+                .forEach(System.out::print);
+
+        Stream.iterate(2, x -> x < 25, x -> x + 6)
+                .forEach(System.out::println);
+
+        Stream.iterate(4, x -> x < 100, x -> x * 4)
+                .forEach(System.out::println);
+
+        Stream.concat(
+                        Stream.of(1, 2, 3, 4),
+                        Stream.of(5, 6, 7, 8))
+                .forEach(System.out::println);
+
+
+        IntStream.range(0, 10)
+                .forEach(System.out::println);
+        LongStream.range(-10l, -5l)
+                .forEach(System.out::println);
 // first lesson
 //        Stream.of(1,2,3)
 //                .forEach(System.out::println);
